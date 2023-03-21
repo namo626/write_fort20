fe55f3e9c5da89aef301dd41324b344ce8ea64f0
Namo <namo26june@gmail.com>
2023-03-21 16:32:02 -0500

    Packaging works

---
+++ setup.py
@@ -0,0 +1,15 @@
+"""Package configuration."""
+from setuptools import find_packages, setup
+
+setup(
+    name="write_fort20",
+    version="0.1",
+    packages=find_packages(),
+    #packages=["src/write_fort20"],
+    #package_dir={"": "src"},
+    install_requires=[],
+    ## Entry point for command line and function to be executed
+    entry_points={
+        'console_scripts': ['write_fort20=write_fort20.fort20write:main'],
+    }
+)
